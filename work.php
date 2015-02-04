dda87210f82df9cb3773eefad2fdc1b5ee44a240
Samuel Kindler <sam@flamov.com>
2015-02-04 16:51:14 +0800

    Added more work projects

--- work.php
+++ work.php
@@ -74,18 +74,56 @@
 
 				array(
 					"photo",
-					"img/index/featured_photo_lion.jpg",
-					"Masai Mara, Kenya",
-					"Animals, Nature, Lions",
-					"album.php"
+					"img/album/hongkong/hongkong_2_full.jpg",
+					"Portfolio",
+					"Landscapes, Nature, Foxes, Lions",
+					"album.php?album=portfolio"
 				),
 
 				array(
 					"photo",
-					"img/index/featured_photo_fox.jpg",
-					"London, United Kingdom",
-					"Animals, Nature, Foxes",
-					"album.php"
+					"img/album/yunnan/yunnan_1_full.jpg",
+					"云南 (Yúnnán), 中国 (China)",
+					"Landscapes, Nature",
+					"album.php?album=yunnan"
+				),
+
+				array(
+					"work",
+					"img/index/featured_work_enicar.jpg",
+					"Enicar",
+					"Complete website re-design for the 100th anniversary of Asia's prominent and longest-lasting timepiece designers.",
+					"Visual &amp; System Design, Frontend & Backend Development, Multilingual Mapping, UAT",
+					"HTML5, CSS3, JavaScript, PHP, Photoshop, Illustrator",
+					"21st September 2014 - 21st September 2014",
+					"project.php"
+				),
+
+				array(
+					"work",
+					"img/index/featured_work_k11.jpg",
+					"Yung's Club",
+					"Creating an inter-connected photobooth solution using a 360° panoramic camera.",
+					"Frontend & Backend Development, PhoneGap/Cordava, REST API",
+					"HTML5, CSS3, JavaScript, PHP, Wordpress, MySQL, Photoshop, Illustrator",
+					"21st September 2014 - 21st September 2014",
+					"project.php"
+				),
+
+				array(
+					"photo",
+					"img/album/kenya/kenya_1_full.jpg",
+					"Kenya, Africa",
+					"Nature, Animals, Lions",
+					"album.php?album=kenya"
+				),
+
+				array(
+					"photo",
+					"img/album/hongkong/hongkong_1_full.jpg",
+					"Hong Kong",
+					"Landscapes, Animals",
+					"album.php?album=hongkong"
 				)
 
 			);
