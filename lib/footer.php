4b3c014266380d4da152e2ac1773ada3394a71b1
Samuel Kindler <sam@flamov.com>
2015-02-12 18:27:27 +0800

    Minor changes

--- lib/footer.php
+++ lib/footer.php
@@ -2,7 +2,7 @@
 
 		<footer id="footer">
 
-			<p>Copyright &copy; Flamov.com 2004-2014.</p>
+			<p>Copyright &copy; Flamov.com 2004-2015.</p>
 			
 			<p class="small">All logos, images and trademarks used on this website are property of their respective owners.</p>
 
