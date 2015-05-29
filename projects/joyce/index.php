827b5bad0633faf80b5cc728cb3a80dcd824a7bc
Samuel Kindler <sam@flamov.com>
2015-05-29 19:05:51 +0800

    Updating Joyce project

--- projects/joyce/index.php
+++ projects/joyce/index.php
@@ -16,9 +16,7 @@
 
 		<p>Joyce is one of Hong Kong's most famous and respected botiques, and they needed a new online identity. The task was to redesign their website from the ground-up for the modern internet, with full responsive and cross-browser compatibility, driven by a powerful backend.</p>
 
-		<p>I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary, I throw myself down among the tall grass by the trickling stream; and, as I lie close to the earth, a thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects and flies, then I feel the presence of the Almighty, who formed us in his own image.</p>
-
-		<p>I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now. When, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary, I throw myself down among the tall grass by the trickling stream; and, as I lie close to the earth, a thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects and flies, then I feel the presence of the Almighty, who formed us in his own image.</p>
+		<p>Furthermore, the site had to be more interactive, such as implementing global search and a directory-system for store and brands related to Joyce.</p>
 
 	</div>
 
