cb71a0a76901013da18a88424e3bf8d75eced2c8
Samuel Kindler <sam@flamov.com>
2015-05-29 19:05:57 +0800

    Small footer typo fix

--- lib/footer.php
+++ lib/footer.php
@@ -4,7 +4,7 @@
 
 			<p>Copyright &copy; Flamov.com 2004-2015.</p>
 			
-			<p class="small">All logos, images and trademarks used on this website are property of their respective owners.</p>
+			<p class="small">All logos, images, and trademarks used on this website are property of their respective owners.</p>
 
 		</footer>
 
