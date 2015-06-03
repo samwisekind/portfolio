997fb105ca8f529edb32c4f1313b733ec173b6f4
Samuel Kindler <sam@flamov.com>
2015-06-03 17:44:37 +0800

    Updated project info (WIP)

--- lib/projects.php
+++ lib/projects.php
@@ -18,7 +18,7 @@
 		"Revamping the online identity of Hong Kong's most famous and respected fashion retailer from the ground-up.",
 		"Visual &amp System Design, Frontend &amp; Backend Development, UAT",
 		"HTML5, CSS3, JavaScript, jQuery, PHP, WordPress, Magento, MySQL, Photoshop, Illustrator",
-		"21st September 2014 - 21st September 2014",
+		"4th April 2014 - 12th February 2015",
 		"http://www.joyce.com/"
 	);
 
@@ -32,60 +32,69 @@
 		"http://www.enicar.com/"
 	);
 
-	$project["steamcast"] = array(
-		array("projects/steamcast/steamcast_cover.jpg", "projects/steamcast/steamcast_cover@2x.jpg"),
-		"Steamcast",
+	$project["steamchat"] = array(
+		array("projects/steamchat/steamchat_cover.jpg", "projects/steamchat/steamchat_cover@2x.jpg"),
+		"Steamchat",
 		"Gaming podcast with 15,000+ monthly listeners and 4,000,000+ annual views, spanning over 100 episodes.",
 		"Visual &amp System Design, Frontend &amp; Backend Development, Audio Editing",
-		"HTML5, CSS3, JavaScript, jQuery, PHP, Photoshop, Illustrator, Audacity, GarageBand, Adobe Soundbooth",
-		"21st September 2014 - 21st September 2014",
-		"http://www.steamcast.com/"
+		"HTML5, CSS3, JavaScript, jQuery, PHP, Photoshop, Illustrator, Audacity, GarageBand, Adobe Soundbooth, Apple Motion",
+		"7th June 2009 - 19th November 2013",
+		"http://www.steamchat.com/"
 	);
 
 	$project["k11"] = array(
-		array("img/index/featured_work_k11.jpg", "img/index/featured_work_k11.jpg"),
+		array("projects/k11/k11_cover.jpg", "projects/k11/k11_cover@2x.jpg"),
 		"K11",
-		"Creating a real-time inter-connected photobooth solution using a 360° panoramic camera.",
+		"Creating a real-time inter-connected photo booth solution using a 360° panoramic camera.",
 		"Visual &amp Systems Design, Frontend &amp; Backend Development, UAT",
-		"HTML5, CSS3, JavaScript, jQuery, REST API, Instagram API, Graph API, Twitter API, Flash, PHP, Photoshop, Illustrator",
+		"HTML5, CSS3, JavaScript, jQuery, REST API, Instagram API, Facebook Graph API, Twitter API, Flash, PHP, Photoshop, Illustrator",
 		"21st September 2014 - 21st September 2014"
 	);
 
 	$project["hkjc"] = array(
-		array("img/index/featured_work_k11.jpg", "img/index/featured_work_k11.jpg"),
+		array("projects/hkjc/hkjc_cover.jpg", "projects/hkjc/hkjc_cover@2x.jpg"),
 		"Hong Kong Jockey Club",
-		"Creating a real-time inter-connected photobooth solution using a 360° panoramic camera.",
+		"Creating a real-time social network photo printing booth.",
 		"Visual &amp Systems Design, Frontend &amp; Backend Development, UAT",
-		"HTML5, CSS3, JavaScript, jQuery, REST API, Instagram API, Graph API, Twitter API, Flash, PHP, Photoshop, Illustrator",
+		"HTML5, CSS3, JavaScript, jQuery, REST API, Wayin, Firefox, Illustrator",
 		"21st September 2014 - 21st September 2014"
 	);
 
 	$project["phonicshero"] = array(
-		array("projects/steamcast/steamcast_cover.jpg", "projects/steamcast/steamcast_cover@2x.jpg"),
+		array("projects/phonicshero/phonicshero_cover.jpg", "projects/phonicshero/phonicshero_cover@2x.jpg"),
 		"Phonics Hero",
-		"Developer of Phonics Hero, a collection of HTML5 canvas and Javascript games for iOS",
-		"Visual &amp System Design, Frontend &amp; Backend Development, Audio Editing",
-		"HTML5, CSS3, JavaScript, jQuery, PHP, Photoshop, Illustrator, Audacity, GarageBand, Adobe Soundbooth",
+		"A collection of HTML5 canvas and JavaScript-powered games for iOS.",
+		"Frontend &amp; Backend Development, UAT",
+		"HTML5, CSS3, JavaScript, jQuery, TweenMax, CreateJS, Cordova, App Store SDK, Xcode, Photoshop, Illustrator",
 		"21st September 2014 - 21st September 2014",
-		"http://www.steamcast.com/"
+		"http://www.phonicshero.com/apps/"
 	);
 
 	$project["yungsclub"] = array(
-		array("projects/steamcast/steamcast_cover.jpg", "projects/steamcast/steamcast_cover@2x.jpg"),
+		array("projects/steamchat/steamchat_cover.jpg", "projects/steamchat/steamchat_cover@2x.jpg"),
 		"Yungs Club",
-		"Gaming podcast with 15,000+ monthly listeners and 4,000,000+ annual views, spanning over 100 episodes.",
+		"Gaming podcast with 15,000+ monthly listeners and 4,000,000+ annual views, spanning over 100 episodes",
 		"Visual &amp System Design, Frontend &amp; Backend Development, Audio Editing",
 		"HTML5, CSS3, JavaScript, jQuery, PHP, Photoshop, Illustrator, Audacity, GarageBand, Adobe Soundbooth",
 		"21st September 2014 - 21st September 2014",
-		"http://www.steamcast.com/"
+		"http://www.steamchat.com/"
 	);
 
 	$project["stevemadden"] = array(
-		array("img/index/featured_work_k11.jpg", "img/index/featured_work_k11.jpg"),
+		array("projects/stevemadden/stevemadden_cover.jpg", "projects/stevemadden/stevemadden_cover@2x.jpg"),
 		"Steve Madden",
-		"Creating a real-time inter-connected photobooth solution using a 360° panoramic camera.",
+		"A photo submission app for Facebook",
 		"Visual &amp Systems Design, Frontend &amp; Backend Development, UAT",
-		"HTML5, CSS3, JavaScript, jQuery, REST API, Instagram API, Graph API, Twitter API, Flash, PHP, Photoshop, Illustrator",
+		"HTML5, CSS3, JavaScript, jQuery, Facebook Graph API, PHP",
+		"23rd May 2014 - 8th July 2014"
+	);
+
+	$project["esm"] = array(
+		array("projects/esm/esm_cover.jpg", "projects/esm/esm_cover@2x.jpg"),
+		"Steve Madden",
+		"A photo submission app for Facebook",
+		"Visual &amp Systems Design, Frontend &amp; Backend Development, UAT",
+		"HTML5, CSS3, JavaScript, jQuery, Facebook Graph API, PHP",
 		"21st September 2014 - 21st September 2014"
 	);
 
