bffe1007b1a2461c600b1aa29e5f815ceb3dc27e
Flamov <sam@flamov.com>
2017-05-13 23:26:46 +0800

    Removed contact

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -1,7 +1,6 @@
 <ul class="nav">
 	<li class="@if ($section === 'project') current @endif"><a href="{{ $app->make('url')->to('/projects') }}"><span>Projects</span></a></li>
 	<li class="@if ($section === 'photography') current @endif"><a href="{{ $app->make('url')->to('/photography') }}"><span>Photography</span></a></li>
-	<li class="@if ($section === 'contact') current @endif"><a href="{{ $app->make('url')->to('/contact') }}"><span>Contact</span></a></li>
 </ul>
 
 <ul class="links">
