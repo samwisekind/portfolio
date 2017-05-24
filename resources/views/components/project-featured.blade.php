8433727dbcb77309c2b6f0000159c2288546507b
Flamov <sam@flamov.com>
2017-05-24 20:36:24 +0800

    Added project link to project list featured component

--- resources/views/components/project-featured.blade.php
+++ resources/views/components/project-featured.blade.php
@@ -6,9 +6,9 @@
 
 			<div class="container">
 
-				<h2><a href="#">{{ $project->title }}</a></h2>
+				<h2><a href="{{ $app->make('url')->to('/projects/' . $project->key) }}">{{ $project->title }}</a></h2>
 
-				<a href="#" class="image">
+				<a href="{{ $app->make('url')->to('/projects/' . $project->key) }}" class="image">
 					<img src="/images/projects/joyce/joyce-featured.png" alt="" />
 				</a>
 
