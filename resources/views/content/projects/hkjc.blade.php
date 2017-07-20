43f28f1be2bbb782e6a118c82d7b85a6b4703483
Flamov <sam@flamov.com>
2017-07-20 23:22:18 +0800

    Updated copywriting on HKJC, Phonics Hero, and Steve Madden project pages

--- resources/views/content/projects/hkjc.blade.php
+++ resources/views/content/projects/hkjc.blade.php
@@ -2,40 +2,29 @@
 
 @section ('project-content')
 
-	<h3>The Idea</h3>
+	<p>We were commissioned to develop a photo printing booth that would use iPads to print photos in real-time. Users would be able to use the iPads to search for and select photos they had posted on Instagram, Facebook, or Twitter. The photo would be sent wirelessly to a printer nearby where it would be printed instantaneously.</p>
 
-	<p>We got commissioned to make a photo printing booth that would use iPad apps to print photos from social media in real-time. Users would be able to use the iPads to search for and select photos they had posted on Instagram, Facebook, and Twitter, and then send it wirelessly to a printer nearby where it would be printed instantaneously.</p>
+	<h3>iPad App</h3>
 
-	<h3>Challenges</h3>
+	<p>We developed the iPad app using <a href="https://cordova.apache.org/" target="_blank" rel="noopener noreferrer">Cordova</a>. The app would send a HTTP <code>GET</code> request to the <a href="http://www.wayin.com" target="_blank" rel="noopener noreferrer">Wayin</a> API service which would return a list of all the social media images of the event posted under a set of hashtags related to the event.</p>
 
-	<p>Although the idea was quite straight-forward, the main challenge was getting the iPad apps to send images to the printer and having them print seamlessly. Furthermore, there had to be a queuing system for the printer as multiple iPads would be able to send images to the printer.</p>
-
-	<h3>Result</h3>
-
-	<h4>iPad App</h4>
-
-	@component("components.project-image")
-		@slot("image", "/images/projects/hkjc/hkjc-image-1.jpg")
-		@slot("caption", "The photo printing booth iPad app. Users can browse through the list of images, search for their username, select the printer under an options menu, and refresh the image list.")
+	@component('components.projects.project-image', ['size' => 'small', 'alt' => 'Screenshot of the HKJC app.'])
+		@slot('image', '/images/projects/hkjc/hkjc-image-1.jpg')
 	@endcomponent
 
-	<p>We developed the iPad app using <a href="https://cordova.apache.org/" target="_blank" rel="noopener noreferrer">Cordova</a> as we planned to create the printing service using a local web-based server. The app would send an AJAX <code>GET</code> request to the <a href="http://www.wayin.com" target="_blank" rel="noopener noreferrer">Wayin</a> API service, which would return a JSON of all the social media images of the event. A JavaScript script would then generate a list from the JSON which would be displayed as a grid of thumbnails. Users could tap on any of the thumbnails, which would show a dialogue box displaying the full image and username. Users could then tap a button and the app would send an AJAX <code>POST</code> request to the printing server with the image URL and printer ID in order for the printing server to decide which printer to print it from.</p>
-
-	<p>Users could also search through the images in real-time using simple client-side <code>regex</code> testing to find their image via their username.</p>
-
-	@component("components.project-image")
-		@slot("image", "/images/projects/hkjc/hkjc-image-2.jpg")
-		@slot("caption", "Tapping a photo will open the printing screen. Tapping the 'Print' button will send the image to the printer.")
+	@component('components.projects.project-caption')
+		@slot('image', '/images/projects/hkjc/hkjc-image-2.jpg')
+		@slot('caption')
+			Tapping the 'print' button would send a HTTP <code>POST</code> request to the printing server with the image URL and a random printer ID.
+		@endslot
 	@endcomponent
 
-	<h4>Printing Server</h4>
+	<h3>Printing Server</h3>
 
-	<p>We ran the printing server using several laptops connected to high-speed photo printers. The server would run a locally-hosted webpage built with <a href="https://www.angularjs.org/" target="_blank" rel="noopener noreferrer">AngularJS</a> that would listen for AJAX requests via the local area network (of which the iPads were connected to). Once the webpage received a request that corresponded to its respective printer ID, it would add the social media image URL to a queue (which also checked for duplicate requests). Once the printer was idle, the social media image would be displayed on the webpage, and a script would use the <code>window.print()</code> function to send the webpage to the printer. We printed the webpage directly as we used a photo frame image as decoration, and therefore used the <code>@media print</code> CSS media query to make sure the dimensions and positions of the photo frame and social media image were sized and aligned correctly.</p>
+	<p>The printing server was run on several laptops connected to high-speed photo printers. The server was built using <a href="https://www.angularjs.org/" target="_blank" rel="noopener noreferrer">AngularJS</a> that would listen for HTTP requests respective to its printer ID on the local area network. Requests would be added to a queue which filtered duplicate requests.</p>
 
-	<p>The printing server ran on Firefox using 'kiosk mode', as it provided advanced client features to enable 'silent printing', which would send print requests to the OS-level printing service without showing any dialogue boxes. Furthermore, Firefox also provides configurations for printing parameters such as sizing and spacing, ideal for printing in specific areas on custom paper sizes.</p>
+	<p>The apps ran in Firefox using the 'kiosk' and 'silent printing' modes, which allowed print requests to be sent to the OS-level printing service without showing any dialogue boxes or notifications. Firefox also allowed for the adjustment of printing parameters such as sizing and spacing, which was cruical to printing on photo-sized paper.</p>
 
-	<h3>Conclusion</h3>
-
-	<p>Overall, the Joyce project was one of my favourites. Being an immense learning experience, it was also a massive undertaking, having been personally responsible for all aspects of design &mdash; including mockups, front-end demos, all CSS and JavaScript development, and UAT &mdash; as well as delving into WordPress and Magento. A lot was learnt about cross-browser compatibility, as having so many different pages with varying layouts and features initially resulted in a lot of interesting problems. Furthermore, it was quite eye-opening being able to work with a variety of different people from the fashion industry, ranging from artists to marketing gurus, and learning how they all view and understand web design, as well as sharing my knowledge and expertise.</p>
+	<p>Printing images was achieved by directly printing a local webpage rendered by AngularJS containing the image next in the queue, and using the native <code>window.print()</code> method. A CSS3 <code>@media print</code> media query ensured the webpage and photo was sized and positioned correctly.</p>
 
 @endsection
