57a1c0104938f0fbb964781dbb7a4770009fd7f5
Flamov <sam@flamov.com>
2017-07-23 21:36:41 +0800

    Updated project caption component style and added max-width value

--- resources/views/components/projects/project-caption.blade.php
+++ resources/views/components/projects/project-caption.blade.php
@@ -3,14 +3,14 @@
 	<div class="media">
 
 		@isset($video)
-			<video autoplay loop muted poster="{{ $image }}" class="video-element">
+			<video autoplay loop muted poster="{{ $image }}" class="video-element" @isset($width) style="max-width: {{ $width . 'px' }}" @endisset>
 				<source src="{{ $video }}.mp4" type="video/mp4">
 				<source src="{{ $video }}.webm" type="video/webm">
 				<source src="{{ $video }}.ogv" type="video/ogg">
 			</video>
 		@endisset
 
-		<img src="{{ $image }}" alt="" class="image-element" />
+		<img src="{{ $image }}" alt="" class="image-element" @isset($width) style="max-width: {{ $width . 'px' }}" @endisset />
 
 	</div>
 
