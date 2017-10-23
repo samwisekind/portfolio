ebba1c980338177e64633069f5ae8a5a9f3c5f03
Flamov <sam@flamov.com>
2017-10-23 20:01:07 +0100

    Updated About page

--- resources/views/layouts/about.blade.php
+++ resources/views/layouts/about.blade.php
@@ -16,11 +16,13 @@
 				<span class="container">Hello!</span>
 			</h2>
 
-			<p class="large">I am Sam, a web developer &amp; designer based in <span class="highlight">Hong Kong</span>. I love obsessing over <span class="highlight red">optimisation</span>, <span class="highlight blue">performance</span>, and <span class="highlight orange">accessibility</span> — using as <span class="highlight red">few bytes &amp; requests</span> as possible to deliver <span class="highlight blue">silky-smooth experiences</span> to as <span class="highlight orange">many people as possible</span>.</p>
+			<p class="large">I am Sam, a web developer &amp; designer currently based in <span class="highlight">London</span>, previously <span class="highlight">Hong Kong</span>. I love obsessing over <span class="highlight red">optimisation</span>, <span class="highlight blue">performance</span>, and <span class="highlight orange">accessibility</span> — using as <span class="highlight red">few bytes &amp; requests</span> to deliver <span class="highlight blue">silky-smooth experiences</span> to as <span class="highlight orange">many people as possible</span>.</p>
 
 			<p>I like to use Vue, Canvas, SVG, SCSS, Node, Gulp, and Laravel, to name a few. Recently I've been working (and having lots of fun!) with <span class="highlight">WebGL</span> and <span class="highlight">three.js</span>.</p>
 
-			<p>If you want to get in touch, feel free to email me at <a href="mailto:sam@flamov.com?subject=Hello!">sam@flamov.com</a>. You can also find me on <a href="https://github.com/Flamov" target="_blank" rel="noopener noreferrer" class="external">GitHub</a>, <a href="https://codepen.io/Flamov" target="_blank" rel="noopener noreferrer" class="external">Codepen</a>, and probably rambling on <a href="https://twitter.com/Flamov" target="_blank" rel="noopener noreferrer" class="external">Twitter</a>.</p>
+			<p>If you want to get in touch, feel free to email me at <a href="mailto:sam@flamov.com?subject=Hello!">sam@flamov.com</a>. You can also find me on <a href="https://github.com/Flamov" target="_blank" rel="noopener noreferrer" class="external">GitHub</a> and <a href="https://codepen.io/Flamov" target="_blank" rel="noopener noreferrer" class="external">Codepen</a>, or probably rambling on <a href="https://medium.com/@Flamov" target="_blank" rel="noopener noreferrer" class="external">Medium</a> and <a href="https://twitter.com/Flamov" target="_blank" rel="noopener noreferrer" class="external">Twitter</a>.</p>
+
+			<p>You can find the source code for this portfolio <a href="https://github.com/Flamov/flamov-portfolio" target="_blank" rel="noopener noreferrer" class="external">here</a>.</p>
 
 			<p>A r&eacute;sum&eacute; is also available upon request.</p>
 
