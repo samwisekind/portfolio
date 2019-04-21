a13ca44827262399fc9d039b284457eeb8445147
Flamov <sam@flamov.com>
2019-04-21 14:00:52 +0100

    Added project page

--- src/controllers/frontend.js
+++ src/controllers/frontend.js
@@ -1,4 +1,5 @@
-const { QUERYFILTER } = require('../helpers/constants');
+const sanitize = require('mongo-sanitize');
+
 const errorHandler = require('../helpers/errorHandler');
 
 const { Project } = require('../models/project');
@@ -6,10 +7,27 @@ const { Project } = require('../models/project');
 const showHome = async (req, res) => {
   try {
     const projects = await Project.find({ enabled: true })
-      .select(QUERYFILTER)
       .sort('order');
 
     res.render('home', { projects });
+  } catch (error) {
+    const { status, message } = errorHandler(error);
+    res.status(status).json({ message });
+  }
+};
+
+const showProject = async (req, res) => {
+  try {
+    const { project } = req.params;
+
+    const result = await Project.findOne({ key: sanitize(project) })
+      .sort('order');
+
+    if (!result) {
+      throw new Error(400);
+    }
+
+    res.render(`projects/${project}`, result);
   } catch (error) {
     const { status, message } = errorHandler(error);
     res.status(status).json({ message });
@@ -36,6 +54,7 @@ const showAbout = async (req, res) => {
 
 module.exports = {
   showHome,
+  showProject,
   showPhotography,
   showAbout,
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,16 @@
 const { Router } = require('express');
 
-const { showHome, showPhotography, showAbout } = require('../controllers/frontend');
+const { showProject, showHome, showPhotography, showAbout } = require('../controllers/frontend');
 const { getAlbums } = require('../controllers/api');
 
 const router = Router();
 
 router.get('/api/photos', getAlbums);
 
+router.get('/', showHome);
+router.get('/projects/:project', showProject);
 router.get('/photography', showPhotography);
 router.get('/about', showAbout);
-router.get('/', showHome);
+
 
 module.exports = router;
