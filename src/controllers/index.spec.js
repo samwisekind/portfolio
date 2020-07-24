f5a7f18c7472f1b364bde62f276c7ad172d1ce12
Flamov <sam@flamov.com>
2020-07-24 22:07:13 +0100

    Add tests for showJournalList

--- src/controllers/index.spec.js
+++ src/controllers/index.spec.js
@@ -19,13 +19,13 @@ const { getFeaturedPhotos } = require('../helpers/photography');
 
 beforeEach(() => {
   getJournalArticlesList.mockImplementation(() => ([
-    { slug: 'test-1', attributes: { title: 'foo', blurb: 'bar', published: '2019-01-01T12:00:00.000Z' }, content: '<p>Lorem ipsum</p>' },
-    { slug: 'test-2', attributes: { title: 'hello', blurb: 'world', published: '2019-05-01T12:00:00.000Z' }, content: '<p>Dolor sit amet</p>' },
+    { slug: 'test-1', attributes: { title: 'foo', blurb: 'bar', published: '2010-01-01T12:00:00.000Z' }, content: '<p>Lorem ipsum</p>' },
+    { slug: 'test-2', attributes: { title: 'hello', blurb: 'world', published: '2020-05-05T12:00:00.000Z' }, content: '<p>Dolor sit amet</p>' },
   ]));
 
   getFeaturedPhotos.mockImplementation(() => ([
     { order: 0, title: 'photo 1', alt: 'photo 1 alt', description: 'photo 1 description', date: '2010', src: 'photo-1-src.jpg' },
-    { order: 1, title: 'photo 2', alt: 'photo 2 alt', description: 'photo 2 description', date: '2020', src: 'photo-2-src.jpg' }
+    { order: 1, title: 'photo 2', alt: 'photo 2 alt', description: 'photo 2 description', date: '2020', src: 'photo-2-src.jpg' },
   ]));
 });
 
@@ -36,10 +36,12 @@ afterEach(() => {
 
 describe('showHome', () => {
   it('shows home', async () => {
-    const response = await request(app)
-      .get('/');
+    const response = await request(app).get('/');
 
     expect(response.status).toBe(200);
+    expect(response.type).toBe('text/html');
+
+    // expect(getJournalArticlesList).toHaveBeenCalledWith(2);
 
     document.body.innerHTML = response.text;
 
@@ -63,5 +65,29 @@ describe('showHome', () => {
     expect(photo2.querySelector('a > img').getAttribute('src')).toBe('photo-2-src.jpg');
     expect(photo2.querySelector('a > img').getAttribute('alt')).toBe('photo 2 alt');
     expect(photo2.querySelector('a > figcaption').textContent).toBe('photo 2 description');
+  });
+});
+
+describe('showJournalList', () => {
+  it('shows journal list', async () => {
+    const response = await request(app).get('/journal');
+
+    expect(response.status).toBe(200);
+    expect(response.type).toBe('text/html');
+
+    document.body.innerHTML = response.text;
+
+    expect(document.body.querySelectorAll('.journal-item').length).toBe(2);
+    const [journal1, journal2] = document.body.querySelectorAll('.journal-item');
+
+    expect(journal1.querySelector('h2 > a').getAttribute('href')).toBe('/journal/test-1');
+    expect(journal1.querySelector('h2 > a').textContent).toBe('foo');
+    expect(journal1.querySelectorAll('p')[0].textContent).toBe('bar');
+    expect(journal1.querySelectorAll('p')[1].textContent).toBe('1 January 2010');
+
+    expect(journal2.querySelector('h2 > a').getAttribute('href')).toBe('/journal/test-2');
+    expect(journal2.querySelector('h2 > a').textContent).toBe('hello');
+    expect(journal2.querySelectorAll('p')[0].textContent).toBe('world');
+    expect(journal2.querySelectorAll('p')[1].textContent).toBe('5 May 2020');
   });
 });
