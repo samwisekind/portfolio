8690536e1473045d8e58afab579dd5843d3214b5
Flamov <sam@flamov.com>
2020-07-29 23:12:12 +0100

    Use native lazy loading

--- src/assets/scripts/global.js
+++ src/assets/scripts/global.js
@@ -2,6 +2,6 @@
 
 import LazyLoad from 'vanilla-lazyload';
 
-const lazyLoad = new LazyLoad({ elements_selector: '.js-lazy' });
+const lazyLoad = new LazyLoad({ elements_selector: '.js-lazy', use_native: true });
 
 export default lazyLoad;
