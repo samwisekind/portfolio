dab4b10fc8ee714a1df4600da04b9dbd68582e28
Flamov <sam@flamov.com>
2020-07-30 22:36:34 +0100

    Update work copy and content

--- src/assets/scripts/global.js
+++ src/assets/scripts/global.js
@@ -4,4 +4,18 @@ import LazyLoad from 'vanilla-lazyload';
 
 const lazyLoad = new LazyLoad({ elements_selector: '.js-lazy', use_native: true });
 
+const workGreeting = document.querySelector('.js-greeting');
+if (workGreeting) {
+  const emojis = workGreeting.getAttribute('data-emojis').split('|');
+
+  let index = 0;
+  const cycleEmoji = () => {
+    index += 1;
+    workGreeting.setAttribute('data-current', emojis[index % emojis.length]);
+  };
+
+  ['webkitAnimationIteration', 'animationiteration']
+    .forEach((event) => workGreeting.addEventListener(event, cycleEmoji));
+}
+
 export default lazyLoad;
