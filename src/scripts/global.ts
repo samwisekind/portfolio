54b60fd749451c58c763c019d27954889b1f4dbb
Flamov <sam@flamov.com>
2021-10-04 00:26:57 +0100

    Updates

--- src/scripts/global.ts
+++ src/scripts/global.ts
@@ -1,39 +1,23 @@
 import LazyLoad from 'vanilla-lazyload';
 
-import { openStars, closeStars } from './components/stars';
-
 const lazyLoad = new LazyLoad({
   elements_selector: '.js-lazy',
   use_native: true,
   unobserve_entered: true,
 });
 
-const workGreeting = document.querySelector('.js-greeting');
-if (workGreeting) {
-  const emojis = workGreeting?.getAttribute('data-emojis')?.split('|') || [];
+const workGreeting = document.querySelector('.js-greeting') as HTMLElement;
+const emojis = (workGreeting.getAttribute('data-emojis') as string).split('|') || [];
 
-  let index = 0;
-  const cycleEmoji = () => {
-    index += 1;
-    workGreeting.setAttribute('data-current', emojis[index % emojis.length]);
-  };
+let index = 0;
+const cycleEmoji = () => {
+  index += 1;
+  workGreeting.setAttribute('data-current', emojis[index % emojis.length]);
+};
 
-  ['webkitAnimationIteration', 'animationiteration']
-    .forEach((event) => workGreeting?.addEventListener(event, cycleEmoji));
-}
-
-document.querySelector('.js-menu-open')?.addEventListener('click', (event) => {
-  event.preventDefault();
-
-  document.body.classList.add('menu');
-  openStars();
-});
-
-document.querySelector('.js-menu-close')?.addEventListener('click', (event) => {
-  event.preventDefault();
-
-  document.body.classList.remove('menu');
-  closeStars();
-});
+[
+  'webkitAnimationIteration',
+  'animationiteration',
+].forEach((event) => workGreeting.addEventListener(event, cycleEmoji));
 
 export default lazyLoad;
